a589fca9f99efdd4e068d333771dbbb756775976
rodolfo-riveros <1001420172@unajma.edu.pe>
2024-06-08 14:15:29 -0500

    Validation

--- PrimerForm/PrimerForm/Form1.cs
+++ PrimerForm/PrimerForm/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,12 +15,25 @@ namespace PrimerForm
         public Form1()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen; // Configurar la posición inicial del formulario
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // Verifica si el texto del TextBox no está vacío o compuesto solo por espacios en blanco
             if (!string.IsNullOrWhiteSpace(TxtName.Text) && !ListName.Items.Contains(TxtName.Text))
+            {
+                // Si el texto no está en el ListBox, lo añade
                 ListName.Items.Add(TxtName.Text);
+
+                // Limpia el TextBox después de intentar agregar el nombre
+                TxtName.Clear();
+            }
+            else
+            {
+                MessageBox.Show("El nombre está vacío o ya existe en la lista.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+
     }
 }
